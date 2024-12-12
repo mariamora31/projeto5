53f58a5317808d69b6d4a57c6c5bd8a58194d9e2
mariamora31 <142507796+mariamora31@users.noreply.github.com>
2024-12-12 20:30:35 -0300

    Add files via upload

---
+++ oli-flash-cards-main/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que é a Lei da Gravitação Universal?',
+    'A Lei da Gravitação Universal, proposta por Isaac Newton, afirma que todo corpo no universo atrai outro corpo com uma força proporcional à massa e inversamente proporcional ao quadrado da distância entre eles.'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma reação química?',
+    'Uma reação química é o processo pelo qual uma ou mais substâncias se transformam em novas substâncias com propriedades diferentes.'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci, um dos maiores artistas do Renascimento italiano.'
+)
+
+criaCartao(
+    'Música',
+    'O que é uma clave de sol?',
+    'A clave de sol é um símbolo musical utilizado para indicar a altura das notas, principalmente para instrumentos como piano e violino.'
+)
